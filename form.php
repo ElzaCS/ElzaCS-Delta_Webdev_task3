2a3645a258cf38ef779a63d0c51b0f20c58da15c
ElzaCS <49568678+ElzaCS@users.noreply.github.com>
2019-07-05 10:38:35 +0300

    Update form.php

--- form.php
+++ form.php
@@ -18,15 +18,15 @@ session_start();
 //invalid login
 if ($_SESSION['user']==null) {
  echo "<h2>PLease login first</h2>";
- echo "<a href='http://proxy/wall.php'><h3>Go to Home Page</h3></a>";
+ echo "<a href='wall.php'><h3>Go to Home Page</h3></a>";
 }
 else {
+
 //new form
 if ($_POST['fname']!=null) {
    //assign form name to session
    $_SESSION['fname']=$_POST['fname'];
- 
-   //add form name&desc to myUsers
+//add form name&desc to myUsers
    $sql="insert into myUsers (name,form_name,form_desc,form_id) values('".$_SESSION['user']."','".$_POST['fname']."','".$_POST['fdesc']."',".$_SESSION['fid'].");"; 
    if ($conn->query($sql) === FALSE) {  echo "<br>shocking: ".$sql."<br>";   }
 }
@@ -41,7 +41,7 @@ if ($_POST['fname']!=null) {
 <div id="id03" class="modal">
   <span onclick="document.getElementById('id03').style.display='none'" 
 class="close" title="Close Modal">&times;</span>
-<form class="modal-content animate" action="http://proxy/login.php" method="post">
+<form class="modal-content animate" action="login.php" method="post">
     <div class="container">
     <label><b>Are you sure you want to leave the form?</b></label>
     <br>
@@ -53,7 +53,7 @@ class="close" title="Close Modal">&times;</span>
 <!-- form to take field labels-->
 <p><h1>Your new form: <?php session_start(); echo $_SESSION['fname'];?></h1></p>
 <center><table width='700px' frame='box' class="tabid" style="position:relative; padding:0;" bgcolor='#b3f0ff'>
-   <form class="newstuff" action="http://proxy/form.php" method="post" style="text-align: left;"> 
+   <form class="newstuff" action="form.php" method="post" style="text-align: left;"> 
     <tr><td><label for="tname"><b>Field Label</b></label></td>
          <td><input type="text" placeholder="enter label(single word, no spaces)" name="txt" style="width:100%;" required></td></tr>
 
@@ -63,7 +63,6 @@ class="close" title="Close Modal">&times;</span>
    </form>
 </table></center>
 <!-- -->
-
 <?php
 //create table "form_formID" for responses
 $sql="create table form_".$_SESSION['fid']." (response_id int(10) unsigned);";
@@ -74,7 +73,8 @@ if ($conn->query($sql) === FALSE) {  //echo "<br>error in: ".$sql."<br>";
 if ($_POST['typ']=="text") {
   $sql="insert into forms (form_id,field_name,field_type) values(".$_SESSION['fid'].",'".$_POST['txt']."','text');"; 
   if ($conn->query($sql) === FALSE) {  echo "<br>error in: ".$sql."<br>";   }
-//add the text field label to responses table
+
+  //add the text field label to responses table
   $sql="alter table form_".$_SESSION['fid']." add column ".$_POST['txt']." varchar(20) not null;";
   if ($conn->query($sql) === FALSE) {  echo "<br>error in: ".$sql."<br>"; }
 }
@@ -88,8 +88,6 @@ if ($_POST['typ']=="number") {
   $sql="alter table form_".$_SESSION['fid']." add column ".$_POST['txt']." varchar(20) not null;";
   if ($conn->query($sql) === FALSE) {  echo "<br>error in: ".$sql."<br>"; }
 }
-
-
 //PRINTING FORM PREVIEW
  $sqlN="select * from myUsers where form_id=".$_SESSION['fid'].";";
 $resN = mysqli_query($conn, $sqlN);
@@ -106,17 +104,16 @@ echo "<tr><td><label>".$rowN['field_name']."</label></td>";
 echo "<td><input type=".$rowN['field_type']." style='width:100%'></input></td></tr>";
 }
 echo "</table><center>"; 
-
 ?>
 <center><button onclick="document.getElementById('id04').style.display='block'" style="height:50px;width:235px;float:center;">Save Form & Get Link</button></center>
 <!-- Save Form & Get Link -->
 <div id="id04" class="modal">
   <span onclick="document.getElementById('id04').style.display='none'" 
 class="close" title="Close Modal">&times;</span>
-<form class="modal-content animate" action="http://proxy/login.php" method="post">
+<form class="modal-content animate" action="login.php" method="post">
     <div class="container">
     <label><b>Form url</b></label>
-    <p>http://proxy/final.php?id=<?php session_start(); echo $_SESSION['fid']; ?></p>
+    <p><a href="final.php?id=<?php session_start(); echo $_SESSION['fid']; ?>">final.php?id=<?php session_start(); echo $_SESSION['fid']; ?></a></p>
     <br>
     <button type="submit" style="height:27px;width:100%;">Complete</button>
   </div>
@@ -126,3 +123,4 @@ class="close" title="Close Modal">&times;</span>
 <?php } ?>
 </body>
 </html>
+
