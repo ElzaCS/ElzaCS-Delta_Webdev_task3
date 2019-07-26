66a8a4b919fe94f173014e1a8d83ad7b8be8efaf
ElzaCS <49568678+ElzaCS@users.noreply.github.com>
2019-07-26 13:23:43 +0300

    Update final.php

--- final.php
+++ final.php
@@ -31,10 +31,29 @@ echo "<tr><td><input type='hidden' id='fid' name='fid' value=".$id."></td></tr>"
  $sqlN="select * from forms where form_id=".$id.";";
  $resN = mysqli_query($conn, $sqlN);
  while($rowN =mysqli_fetch_array($resN, MYSQLI_ASSOC)) {
-
+if ($rowN['field_type']=="image") {
+    echo "<tr><td><img src='".$rowN['field_name']."' widht='200' height='200'></td>";
+}
+else if ($rowN['field_type']=="radio") {
+    echo "<tr><td><img src='".$rowN['field_name']."</label></td></tr>";
+    if ($rowN['name1']!=null) echo "<input type='radio' name='".$rowN['field_name']."' value='".$rowN['name1']."'>".$rowN['name1'];
+    if ($rowN['name2']!=null) echo "<input type='radio' name='".$rowN['field_name']."' value='".$rowN['name2']."'>".$rowN['name2'];
+    if ($rowN['name3']!=null) echo "<input type='radio' name='".$rowN['field_name']."' value='".$rowN['name3']."'>".$rowN['name3'];
+    if ($rowN['name4']!=null) echo "<input type='radio' name='".$rowN['field_name']."' value='".$rowN['name4']."'>".$rowN['name4'];
+    echo "</td></tr>";
+}
+else if ($rowN['field_type']=="dropdown") {
+    echo "<tr><td><label>".$rowN['field_name']."</label></td><td><select name='".$rowN['field_name']."'>";
+    if ($rowN['name1']!=null) echo "<option value'".$rowN['name1']."'>".$rowN['name1']."</option>";
+    if ($rowN['name2']!=null) echo "<option value'".$rowN['name2']."'>".$rowN['name2']."</option>";
+    if ($rowN['name3']!=null) echo "<option value'".$rowN['name3']."'>".$rowN['name3']."</option>";
+    if ($rowN['name4']!=null) echo "<option value'".$rowN['name4']."'>".$rowN['name4']."</option>";
+    echo "<select></td>";
+}
+else {
 echo "<tr><td><label>".$rowN['field_name']."</label></td>";
 echo "<td><input type=".$rowN['field_type']." style='width:100%' name='".$rowN['field_name']."' required></input></td></tr>";
-}
+}}
 session_start();
    $rand=rand();
    $_SESSION['rid']=$rand;
