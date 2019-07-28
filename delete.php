41e961f640c6581e2dc8a0eb2bf1108e32f65633
ElzaCS <49568678+ElzaCS@users.noreply.github.com>
2019-07-28 12:48:47 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,23 +1,25 @@
 <?php
-$field=$_REQUEST['field'];
-$type=$_REQUEST['type'];
+$field = $_REQUEST['field'];
+$type = $_REQUEST['type'];
 session_start();
 $fid=$_SESSION['fid'];
 
-$servername="localhost";
-$username="root";
-$password="redhat";
-$dbname="wallDB";
+$servername = "localhost";
+$username = "root";
+$password = "redhat";
+$dbname = "wallDB";
 
-//Create connection
-$conn=new mysqli($servername,$username,$password,$dbname);
-//Check connection
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
 if ($conn->connect_error) {
-  die("Connection failed:".$conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
+//echo "fid=".$fid.", field=".$field.", type=".$type;
 
 //delete from db
-$sql="delete from forms where form_id=".$fid." and field_name='".$field."' and field_type='"..$type"';";
-if ($conn->query($sql)===FALSE) {}
+$sql="delete from forms where form_id=".$fid." and field_name='".$field."' and field_type='".$type."';";
+  if ($conn->query($sql) === FALSE) {  //echo "<br>error in: ".$sql."<br>";   
+  }
 header("Location: form.php");
 ?>
