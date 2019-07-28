5ed6568256003202a08daefa53ff9db30213adc4
ElzaCS <49568678+ElzaCS@users.noreply.github.com>
2019-07-28 12:51:22 +0300

    Update time.php

--- time.php
+++ time.php
@@ -1,28 +1,35 @@
 <?php
-$t1=$_POST['time1'];
-$t2=$_POST['time2'];
-$t3=$_POST['time3'];
+$t1 = $_POST['time1'];
+$t2 = $_POST['time2'];
+$t3 = $_POST['time3'];
+//echo $t1."<br>".$t2."<br>".$t3."<br>";
 
 session_start();
+//echo $_SESSION['fid']."<br>";
 
-$servername="localhost";
-$username="root";
-$password="redhat";
-$dbname="wallDB";
 
-//Create connection
-$conn=new mysqli($servername,$username,$password,$dbname);
-//check connection
+
+ $servername = "localhost";
+$username = "root";
+$password = "redhat";
+$dbname = "wallDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
 if ($conn->connect_error) {
-  die("Connection failed".$conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 //add to db
-$sql="update myUsers set date=".$t1". where form_id=".$_SEESION['fid'];
-if ($conn->query($sql)===FALSE) {}
-$sql="update myUsers set month=".$t2". where form_id=".$_SEESION['fid'];
-if ($conn->query($sql)===FALSE) {}
-$sql="update myUsers set year=".$t3". where form_id=".$_SEESION['fid'];
-if ($conn->query($sql)===FALSE) {}
+$sql="update myUsers set date=".$t1." where form_id=".$_SESSION['fid']."";
+if ($conn->query($sql) === FALSE) {  //echo "<br>error in: ".$sql."<br>";   
+  } //else echo "true";
+$sql="update myUsers set month=".$t2." where form_id=".$_SESSION['fid']."";
+if ($conn->query($sql) === FALSE) {  //echo "<br>error in: ".$sql."<br>";   
+  } //else echo "true";
+$sql="update myUsers set year=".$t3." where form_id=".$_SESSION['fid']."";
+if ($conn->query($sql) === FALSE) {  //echo "<br>error in: ".$sql."<br>";   
+  } //else echo "true";
 header("Location: form.php");
 ?>
